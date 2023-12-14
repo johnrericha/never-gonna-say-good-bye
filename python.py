50c340357da5f23693638e29bd858181a00fb597
johnrericha <honzikR95@seznam.cz>
2023-12-14 16:30:02 +0100

    python add

---
+++ python.py
@@ -0,0 +1,12 @@
+import random
+import sys
+robot_choice = random.randint(1,3)
+print("1) budes chudy člověk")
+print("2) budes bohaty")
+print("3)udes mit normalni zivot")
+if x == 1:
+print("prohrál jsi")
+if z == 2:
+print("vyhrál jsi")
+if y == 3:
+print("budes v pohode")
